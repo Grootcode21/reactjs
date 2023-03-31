764aca6d7ca8142565c7f354256a4ce79cbb6391
Grootcode21 <antonyndichu15@gmail.com>
2023-03-30 23:39:53 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,36 +1,10 @@
-import './App.css';
+import {GrNotes} from "react-icons/gr"
 
-const tahoe_peaks = [
-  { name: "Freel", elevation: 10891},
-  { name: "Monument", elevation: 10067},
-  { name: "Pyramid", elevation: 9983},
-  { name: "Tallac", elevation: 9735},
-];
-
-function List({data, renderItem, renderEmpty}){
-  return !data.length ? (
-    renderEmpty
-  ) : (
-    <ul>
-      {data.map((item) =>(
-        <li key={item.name}>
-          {renderItem(item)}
-        </li>
-      ))}
-    </ul>
-  )
-}
 function App() {
   return (
-  <List
-    data={tahoe_peaks}
-    renderEmpty={<p>This list is empty</p>}
-    renderItem={(item) => (
-    <>
-     {item.name} - {item.elevation} ft.
-    </>
-  )}
-  />
+    <div>
+      <h1><GrNotes />My Appointments</h1>
+    </div>
   );
 }
 
