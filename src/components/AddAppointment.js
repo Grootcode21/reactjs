b5bdee6c7e2bcc81499ccabac613397493b1a687
Grootcode21 <antonyndichu15@gmail.com>
2023-04-07 23:58:24 -0700

    Appointment form styling

--- src/components/AddAppointment.js
+++ src/components/AddAppointment.js
@@ -34,55 +34,55 @@ const AddAppointment = ({onSendAppointment, lastId}) => {
           toggleForm && 
           <div className="border-r-2 border-b-2 border-l-2 border-light-blue-500 rounded-b-md pl-4 pr-4 pb-4">
           <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start  sm:pt-5">
-            <label htmlFor="ownerName" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+            <label htmlFor="ownerName" className="block text-md font-medium text-gray-700 sm:mt-px sm:pt-2">
               Owner Name
             </label>
             <div className="mt-1 sm:mt-0 sm:col-span-2">
               <input type="text" name="ownerName" id="ownerName"
               onChange={(event) => {setFormData({ ...formData, ownerName: event.target.value})}}
               value={formData.ownerName}
-                className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md" />
+                className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs lg:text-lg border-gray-300 rounded-md" />
             </div>
           </div>
   
           <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start  sm:pt-5">
-            <label htmlFor="petName" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+            <label htmlFor="petName" className="block text-md font-medium text-gray-700 sm:mt-px sm:pt-2">
               Pet Name
             </label>
             <div className="mt-1 sm:mt-0 sm:col-span-2">
               <input type="text" name="petName" id="petName"
               onChange={(event) => {setFormData({ ...formData, petName: event.target.value})}}
               value={formData.petName}
-                className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md" />
+                className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs lg:text-lg border-gray-300 rounded-md" />
             </div>
           </div>
   
           <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start  sm:pt-5">
-            <label htmlFor="aptDate" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+            <label htmlFor="aptDate" className="block text-md font-medium text-gray-700 sm:mt-px sm:pt-2">
               Apt Date
             </label>
             <div className="mt-1 sm:mt-0 sm:col-span-2">
               <input type="date" name="aptDate" id="aptDate"
               onChange={(event) => {setFormData({ ...formData, aptDate: event.target.value})}}
               value={formData.aptDate}
-                className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md" />
+                className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs lg:text-lg border-gray-300 rounded-md" />
             </div>
           </div>
   
           <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start  sm:pt-5">
-            <label htmlFor="aptTime" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+            <label htmlFor="aptTime" className="block text-md font-medium text-gray-700 sm:mt-px sm:pt-2">
               Apt Time
             </label>
             <div className="mt-1 sm:mt-0 sm:col-span-2">
               <input type="time" name="aptTime" id="aptTime"
               onChange={(event) => {setFormData({ ...formData, aptTime: event.target.value})}}
               value={formData.aptTime}
-                className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md" />
+                className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs lg:text-lg border-gray-300 rounded-md" />
             </div>
           </div>
   
           <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start  sm:pt-5">
-            <label htmlFor="aptNotes" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+            <label htmlFor="aptNotes" className="block text-md font-medium text-gray-700 sm:mt-px sm:pt-2">
               Appointment Notes
             </label>
             <div className="mt-1 sm:mt-0 sm:col-span-2">
